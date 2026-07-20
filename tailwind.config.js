d9057b55411515b3bb42bd4a5a6653be0635e7b5
luckydyu <luckydyu@gmail.com>
2026-07-21 01:41:50 +0900

    fix: 화면 레이아웃 깨짐과 외부 CSS 의존성 제거

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        forest: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          200: '#bbf7d0',
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+          950: '#022c22'
+        }
+      }
+    }
+  }
+};
